39359c3821aa30ae31f44da5d511bcbfebe9c2c0
Alexander Fu <aafu@ucdavis.edu>
2016-08-01 23:39:35 -0700

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -953,7 +953,7 @@ app.service('UserService', function() {
         $.ajax({
             type: 'GET',
             contentType: 'application/json',
-            url: 'http://pokemon.rhocode.com:81/api/getPokemon/' + id,
+            url: 'https://pokemon.rhocode.com:81/api/getPokemon/' + id,
             success: function(data) {
 
                 var dat = JSON.parse(JSON.stringify(data));
